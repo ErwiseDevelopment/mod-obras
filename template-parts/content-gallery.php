95fd0f09f2ebb494742aa96e7857e69be5011aea
ErwiseDevelopment <erwisedev@gmail.com>
2022-08-08 00:02:15 -0300

    obras

---
+++ template-parts/content-gallery.php
@@ -0,0 +1,63 @@
+<section class="l-gallery pb-5">
+    
+    <div class="container">
+
+        <div class="row justify-content-center">
+
+            <div class="col-10">
+
+                <div class="row">
+                    
+                    <div class="col-12">
+
+                        <div class="row">
+                            
+                            <!-- loop -->
+                            <?php
+                                $args = array(
+                                    'posts_per_page' => 8,
+                                    'post_type'      => 'fotos',
+                                    'order'          => 'DESC'
+                                );
+
+                                $galleries = new WP_Query( $args );
+
+                                if( $galleries->have_posts() ) :
+                                    while( $galleries->have_posts() ) : $galleries->the_post();
+                            ?>
+                                        <div class="col-md-4 my-3">
+                                            
+                                            <a 
+                                            class="l-gallery__album d-block"
+                                            href="<?php the_permalink() ?>">
+                                                <img
+                                                class="img-fluid w-100"
+                                                src="<?php echo get_field( 'capa_do_album' ) ?>"
+                                                alt="<?php the_title() ?>">
+                                            </a>
+                                        </div>
+                            <?php   endwhile;
+                                endif;
+
+                                wp_reset_query();
+                            ?>
+                            <!-- end loop -->
+                        </div>
+
+                        <div class="row justify-content-center">
+
+                            <div class="col-md-4 col-lg-3 mt-md-5">
+
+                                <a 
+                                class="l-news__small__card-read-more u-line-height-100 hover:u-opacity-8 d-block u-font-weight-bold text-center text-decoration-none u-color-folk-white u-bg-folk-theme py-2 px-5" 
+                                href="<?php echo get_home_url( null, 'galeria' ) ?>">
+                                    Mais fotos
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ template-parts/content-new-home-about.php
@@ -0,0 +1,61 @@
+<section class="l-new-home-about my-5">
+
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-12">
+
+                <div class="row">
+
+                    <div class="col-lg-5 pl-lg-0"> 
+                        <!-- <img
+                        class="img-fluid h-100"
+                        src="<php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/new-home-about-image-1.png"
+                        alt=""> -->
+
+                        <img
+                        class="img-fluid h-100"
+                        src="<?php echo get_field( 'imagem_quem_somos' ) ?>"
+                        alt="Quem Somos">
+                    </div>
+
+                    <div class="col-lg-5 mt-3 mt-md-0">
+
+                        <h3 class="u-title--highlight u-font-weight-bold text-uppercase all:u-color-folk-theme mb-4">
+                            quem <span class="u-font-weight-black">somos</span>
+                        </h3>
+
+                        <!-- <p class="l-new-home-about__text u-font-weight-medium text-justify">
+                            O Parque Dom Bosco é uma obra social da Rede
+                            Salesiana Brasil, que atua na cidade de Itajaí na
+                            educação e evangelização de crianças, adolescentes e jovens <br>
+
+                            <br>
+
+                            Temos 60 anos de serviços prestados a comunidade
+                            Itajaiense atuando na educação e evangelização de
+                            crianças, adolescentes e jovens. Sinta-se em casa, você
+                            será sempre bem-vindo!
+                        </p> -->
+
+                        <span class="l-new-home-about__text d-block u-font-weight-medium text-justify">
+                            <?php echo get_field( 'descricao_quem_somos' ) ?>
+                        </span>
+
+                        <div class="row justify-content-end">
+
+                            <div class="col-md-5">
+                                <a 
+                                class="l-news__small__card-read-more u-line-height-100 hover:u-opacity-8 d-block u-font-weight-bold text-center text-decoration-none u-color-folk-white u-bg-folk-primary py-2 px-5" 
+                                href="<?php echo get_field( 'saiba_mais_quem_somos' ) ?>">
+                                    Saiba mais
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
